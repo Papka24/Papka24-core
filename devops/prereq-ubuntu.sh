135275bda8d482de40d768b1f52dd532884f4da5
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-06-27 13:50:29 +0300

    docker-compose skeleton

---
+++ devops/prereq-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y mc
+
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
