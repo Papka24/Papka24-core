4cd673cba396c05d92f1a2f12d0b90fcc5c46c2b
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-06-27 13:48:11 +0000

    httpd/postgres/redis/openjdk images, connected redis db

--- devops/prereq-ubuntu.sh
+++ devops/prereq-ubuntu.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y default-jdk
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
+    gradle \
     curl \
     software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -22,3 +23,9 @@ sudo apt-get install -y docker-ce
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
+sudo apt-get install gradle-4.0
+
+sudo apt-get install -y nodejs npm
--- up.sh
+++ up.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-docker-compose up
 
+pushd static-page
+gradle build
+popd
+
+pushd server
+gradle shadowJar
+popd
+
+rm -R ./devops/static/dist
+mkdir ./devops/static/dist
+cp -R ./static-page/papka/* ./devops/static/dist
+
+rm -R ./devops/server/dist
+mkdir ./devops/server/dist
+cp -R ./server/build/libs/papka-24.jar ./devops/server/dist
+
+docker-compose up -d
