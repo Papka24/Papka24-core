2d6588fc00f99188e9b28ffd2de2e40d62191bb2
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-06-28 09:32:18 +0000

    Server domain property

--- up.sh
+++ up.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+sed -i "/server.domain=/ s/=.*/=1111/" ./server/src/main/resources/config.properties
+
 : '
 pushd static-page
 gradle build
 popd
-'
+
 pushd server
 gradle shadowJar
 popd
-: '
+
 rm -R ./devops/static/dist
 mkdir ./devops/static/dist
 cp -R ./static-page/papka/* ./devops/static/dist
-'
+
 rm -R ./devops/server/dist
 mkdir ./devops/server/dist
 cp ./server/build/libs/papka-24.jar ./devops/server/dist
@@ -22,4 +24,4 @@ mkdir ./devops/postgres/dist
 cp ./server/build/resources/main/sql/CreateDB.sql ./devops/postgres/dist
 
 docker-compose up
-
+'
